820d2a03236693a44d7ad0212b5200753f7f6a60
Natthan <natthandosantos@gmail.com>
2024-10-31 10:52:00 -0300

    Set env with httptools library

--- server/app.py
+++ server/app.py
@@ -1,11 +1,18 @@
 import socket
+from httptools import HttpRequestParser
 
-from http_parser.http import HttpStream
-from http_parser.reader import SocketReader
+class RequestHandler:
+    def __init__(self):
+        self.parser = HttpRequestParser(self)
+    
+    def on_url(self, url):
+        print(f"URL recebida: {url.decode()}")
 
 def main():
-    print(f'test')
-
+    print("Testando o parsing HTTP")
+    handler = RequestHandler()
+    data = b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
+    handler.parser.feed_data(data)
 
 if __name__ == '__main__':
     main()
