b055c579a519ad199426366e8de9846da63be798
Artur da Silva <arturmariano004@gmail.com>
2024-11-14 21:12:12 -0300

    Alterações 07/11

--- app.py
+++ app.py
@@ -1,26 +1,75 @@
 from server.Server import Server
 from httptools import HttpRequestParser
+from server.File import File
 
 class RequestHandler:
-    def __init__(self):
+    def __init__(self, client_socket):
+        self.client_socket = client_socket
         self.parser = HttpRequestParser(self)
-    
+        self.url = None
+
     def on_url(self, url):
-        print(f"URL recebida: {url.decode()}")
+        # Extrai e decodifica a URL
+        self.url = url.decode()
+        print(f"URL recebida: {self.url}")
+    
+    def on_headers_complete(self):
+        # Verifica se é uma requisição GET
+        if self.url and self.url.startswith("/arquivos/"):
+            file_name = self.url.split("/arquivos/")[-1]
+            self.handle_get(file_name)
+
+    def handle_get(self, file_name):
+        # Interage com o servidor para buscar o arquivo
+        file_storage = {
+            "doc1": File('doc1', 'Conteúdo do documento 1', 'text/plain'),
+            "doc2": File('doc2', 'Conteúdo do documento 2', 'text/plain')
+        }
+
+        if file_name in file_storage:
+            file = file_storage[file_name]
+            content = file.content
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                f"Content-Type: {file.file_type}\r\n"
+                "Content-Length: " + str(len(content)) + "\r\n"
+                "Connection: close\r\n\r\n" +
+                content
+            )
+        else:
+            response = (
+                "HTTP/1.1 404 Not Found\r\n"
+                "Content-Type: text/plain\r\n"
+                "Connection: close\r\n\r\n"
+                "Error: File not found"
+            )
+
+        self.client_socket.send(response.encode('utf-8'))
+        self.client_socket.close()
 
 def main():
-    # print("Testando o parsing HTTP")
-    # handler = RequestHandler()
-    # data = b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n"
-    # handler.parser.feed_data(data)
+    # Inicializa o servidor
     serv = Server('localhost', 80)
     socket = serv.create_socket('TCP')
     if not socket:
         print('ERROR: Only TCP allowed')
+        return
     serv.bind_socket(socket)
 
-    
+    # Inicia o servidor e processa as requisições
+    socket.listen(5)
+    print("Server is listening on port 80")
 
+    while True:
+        client_socket, _ = socket.accept()
+        request_handler = RequestHandler(client_socket)
+        
+        # Recebe a requisição e processa com HttpRequestParser
+        data = client_socket.recv(2048)
+        if data:
+            request_handler.parser.feed_data(data)
+        else:
+            client_socket.close()
 
 if __name__ == '__main__':
     main()
--- server/Server.py
+++ server/Server.py
@@ -1,21 +1,24 @@
 import socket
-from File import File
+from server.File import File
 
-# file = File('doc1', 'asdansodnas', 'text/type')
-
-# print(f'Created file: {file.name}')
+# Simulando armazenamento de arquivos em memória
+file_storage = {
+    "doc1": File('doc1', 'Conteúdo do documento 1', 'text/plain'),
+    "doc2": File('doc2', 'Conteúdo do documento 2', 'text/plain')
+}
 
 class Server:
-    def __init__(self, host, port, data_payload = 2048):
+    def __init__(self, host, port, data_payload=2048):
         self.host = host
         self.port = port
         self.data_payload = data_payload
 
+    @staticmethod
     def create_socket(type):
-        if type is 'TCP':
-            # Create a TCP socket
-            sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-            # Enable reuse address/port 
+        if type == 'TCP':
+            # Criando um socket TCP
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # Habilita reuso de endereço/porta
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             return sock
         else:
@@ -23,9 +26,58 @@ class Server:
         
     def bind_socket(self, sock):
         server_address = (self.host, self.port)
-        print ("Starting up echo server  on %s port %s" % server_address)
+        print("Starting up server on %s port %s" % server_address)
         sock.bind(server_address)
 
-    
+    def handle_request(self, client_socket):
+        # Recebe a requisição do cliente
+        request = client_socket.recv(self.data_payload).decode('utf-8')
+        
+        if request.startswith("GET"):
+            # Extrai o nome do arquivo a partir do caminho na requisição
+            try:
+                request_line = request.splitlines()[0]
+                method, path, _ = request_line.split()
+                file_name = path.split("/arquivos/")[-1] if "/arquivos/" in path else None
+                print(file_name)
+                
+                # Busca o arquivo em memória
+                if file_name in file_storage:
+                    content = file.content  # Acessa o conteúdo do arquivo
+                    response = (
+                        "HTTP/1.1 200 OK\r\n"
+                        f"Content-Type: {file.file_type}\r\n"
+                        "Content-Length: " + str(len(content)) + "\r\n"
+                        "Connection: close\r\n\r\n" +
+                        content
+                    )
+                else:
+                    # Arquivo não encontrado
+                    response = (
+                        "HTTP/1.1 404 Not Found\r\n"
+                        "Content-Type: text/plain\r\n"
+                        "Connection: close\r\n\r\n"
+                        "Error: File not found"
+                    )
 
-    
+                # Envia a resposta
+                client_socket.send(response.encode('utf-8'))
+                print(f"Served file '{file_name}' with status: {'200 OK' if file_name in file_storage else '404 Not Found'}")
+            except Exception as e:
+                print("Error processing request:", e)
+        
+        # Fecha a conexão
+        client_socket.close()
+
+    def start_server(self):
+        # Cria e configura o socket do servidor
+        sock = self.create_socket('TCP')
+        if sock:
+            self.bind_socket(sock)
+            sock.listen(5)
+            print("Server is listening...")
+
+            while True:
+                print("Waiting to receive request from client")
+                client_socket, address = sock.accept()
+                self.handle_request(client_socket)
